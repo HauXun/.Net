51c92fcd759b01ff5be1d0437c763ce0a2befdb4
HauXun <hautrieu65@gmail.com>
2021-08-30 22:55:50 +0700

    Licenses Bunify Framework

--- Projects/Winform Projects/Small Projects/QuanLyQuanCafe/QuanLyQuanCafe/fMain.Designer.cs
+++ Projects/Winform Projects/Small Projects/QuanLyQuanCafe/QuanLyQuanCafe/fMain.Designer.cs
@@ -30,6 +30,10 @@ namespace QuanLyQuanCafe
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+			Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderEdges borderEdges1 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderEdges();
+			Bunifu.UI.WinForms.BunifuButton.BunifuButton.StateProperties stateProperties1 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton.StateProperties();
+			Bunifu.UI.WinForms.BunifuButton.BunifuButton.StateProperties stateProperties2 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton.StateProperties();
 			this.btnLogin = new System.Windows.Forms.Button();
 			this.btnExit = new System.Windows.Forms.Button();
 			this.label1 = new System.Windows.Forms.Label();
@@ -38,6 +42,7 @@ namespace QuanLyQuanCafe
 			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
 			this.errorProviderWar = new System.Windows.Forms.ErrorProvider(this.components);
+			this.bunifuButton1 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton();
 			((System.ComponentModel.ISupportInitialize)(this.errorProviderWar)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -115,6 +120,69 @@ namespace QuanLyQuanCafe
 			// 
 			this.errorProviderWar.ContainerControl = this;
 			// 
+			// bunifuButton1
+			// 
+			this.bunifuButton1.AllowToggling = false;
+			this.bunifuButton1.AnimationSpeed = 200;
+			this.bunifuButton1.AutoGenerateColors = false;
+			this.bunifuButton1.BackColor = System.Drawing.Color.Transparent;
+			this.bunifuButton1.BackColor1 = System.Drawing.Color.DodgerBlue;
+			this.bunifuButton1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuButton1.BackgroundImage")));
+			this.bunifuButton1.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+			this.bunifuButton1.ButtonText = "bunifuButton1";
+			this.bunifuButton1.ButtonTextMarginLeft = 0;
+			this.bunifuButton1.ColorContrastOnClick = 45;
+			this.bunifuButton1.ColorContrastOnHover = 45;
+			this.bunifuButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+			borderEdges1.BottomLeft = true;
+			borderEdges1.BottomRight = true;
+			borderEdges1.TopLeft = true;
+			borderEdges1.TopRight = true;
+			this.bunifuButton1.CustomizableEdges = borderEdges1;
+			this.bunifuButton1.DialogResult = System.Windows.Forms.DialogResult.None;
+			this.bunifuButton1.DisabledBorderColor = System.Drawing.Color.Empty;
+			this.bunifuButton1.DisabledFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+			this.bunifuButton1.DisabledForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(160)))), ((int)(((byte)(168)))));
+			this.bunifuButton1.FocusState = Bunifu.UI.WinForms.BunifuButton.BunifuButton.ButtonStates.Pressed;
+			this.bunifuButton1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F);
+			this.bunifuButton1.ForeColor = System.Drawing.Color.White;
+			this.bunifuButton1.IconLeftCursor = System.Windows.Forms.Cursors.Hand;
+			this.bunifuButton1.IconMarginLeft = 11;
+			this.bunifuButton1.IconPadding = 10;
+			this.bunifuButton1.IconRightCursor = System.Windows.Forms.Cursors.Hand;
+			this.bunifuButton1.IdleBorderColor = System.Drawing.Color.DodgerBlue;
+			this.bunifuButton1.IdleBorderRadius = 3;
+			this.bunifuButton1.IdleBorderThickness = 1;
+			this.bunifuButton1.IdleFillColor = System.Drawing.Color.DodgerBlue;
+			this.bunifuButton1.IdleIconLeftImage = null;
+			this.bunifuButton1.IdleIconRightImage = null;
+			this.bunifuButton1.IndicateFocus = false;
+			this.bunifuButton1.Location = new System.Drawing.Point(60, 47);
+			this.bunifuButton1.Name = "bunifuButton1";
+			stateProperties1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(181)))), ((int)(((byte)(255)))));
+			stateProperties1.BorderRadius = 3;
+			stateProperties1.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+			stateProperties1.BorderThickness = 1;
+			stateProperties1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(181)))), ((int)(((byte)(255)))));
+			stateProperties1.ForeColor = System.Drawing.Color.White;
+			stateProperties1.IconLeftImage = null;
+			stateProperties1.IconRightImage = null;
+			this.bunifuButton1.onHoverState = stateProperties1;
+			stateProperties2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(96)))), ((int)(((byte)(144)))));
+			stateProperties2.BorderRadius = 3;
+			stateProperties2.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+			stateProperties2.BorderThickness = 1;
+			stateProperties2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(96)))), ((int)(((byte)(144)))));
+			stateProperties2.ForeColor = System.Drawing.Color.White;
+			stateProperties2.IconLeftImage = null;
+			stateProperties2.IconRightImage = null;
+			this.bunifuButton1.OnPressedState = stateProperties2;
+			this.bunifuButton1.Size = new System.Drawing.Size(210, 45);
+			this.bunifuButton1.TabIndex = 7;
+			this.bunifuButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.bunifuButton1.TextMarginLeft = 0;
+			this.bunifuButton1.UseDefaultRadiusAndThickness = true;
+			// 
 			// fMain
 			// 
 			this.AcceptButton = this.btnLogin;
@@ -122,6 +190,7 @@ namespace QuanLyQuanCafe
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.btnExit;
 			this.ClientSize = new System.Drawing.Size(327, 172);
+			this.Controls.Add(this.bunifuButton1);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.tbPassword);
 			this.Controls.Add(this.label2);
@@ -151,5 +220,6 @@ namespace QuanLyQuanCafe
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.ErrorProvider errorProviderWar;
+		private Bunifu.UI.WinForms.BunifuButton.BunifuButton bunifuButton1;
 	}
 }
